0382eda7cf51a2c6e532beb65b3182ea9a468c36
Sheeba Ali <sheeba.ali@novusaltair.com>
2025-01-15 10:38:33 +0500

    updates

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://job-portal2-ten.vercel.app",
+    origin: "https://job-portal-gth27kd7j-sheebaali01s-projects.vercel.app",
     credentials: true
 }));
 
